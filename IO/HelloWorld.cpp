75dabfbcc2d57fbfd8c12c98babc066c462d85b8
Jensen <jinxin023@yeah.net>
2025-01-11 10:51:23 +0800

    helloWorld

---
+++ IO/HelloWorld.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+// io stream package
+using namespace std;
+// 使用了std这个命名空间
+// 主函数
+int main(){
+    // 运算符 <<
+    // std::cout 库中函数
+    // std:: 命名空间
+    // <<std::endl 输出行结束
+    std::cout<< "Hello World" << std::endl;
+    return 0;
+}
