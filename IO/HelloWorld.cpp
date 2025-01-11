2b6cabb1926f710c15414c8527bcfb801d15393c
Jensen <jinxin023@yeah.net>
2025-01-11 13:57:24 +0800

    add code runner

--- IO/HelloWorld.cpp
+++ IO/HelloWorld.cpp
@@ -9,6 +9,18 @@ int main(){
     // std:: 命名空间
     // <<std::endl 输出行结束
     std::cout<< "Hello World" << std::endl;
+
+    // input the name
+    std::cout<< "Please input your name: ";
+
+    // 定义一个字符串
+    std::string name;
+
+    // 输入字符串
+    std::cin >> name;
+
+    // 输出字符串
+    std::cout<< "Hello, " << name << "!!!" << std::endl;
     return 0;
 }
 // 再接再厉
